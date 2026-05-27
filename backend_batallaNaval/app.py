06249b05a83cfd3132f65f9d632439f0f9f8f1bf
MariaAngarita13 <149028254+MariaAngarita13@users.noreply.github.com>
2026-05-27 07:49:05 -0500

    Modify CORS and app run configuration
    Updated CORS settings to allow all origins and modified app run configuration to use environment variable for port.

--- backend_batallaNaval/app.py
+++ backend_batallaNaval/app.py
@@ -1,24 +1,19 @@
 # backend_batallaNaval/app.py
-
 from flask import Flask
 from flask_cors import CORS
-
 from extensions import cache
 from routes.usuarios_routes import usuarios_bp
 from routes.partidas import partidas_bp
 from routes.login_routes import login_bp
+import os
 
 app = Flask(__name__)
 
 # ─── CORS ───
-CORS(app, resources={r"/*": {"origins": [
-    "http://localhost:4200",
-    "http://localhost:4201",
-]}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.config["CACHE_TYPE"]            = "SimpleCache"
 app.config["CACHE_DEFAULT_TIMEOUT"] = 60
-
 cache.init_app(app)
 
 app.register_blueprint(usuarios_bp)
@@ -26,4 +21,5 @@ app.register_blueprint(partidas_bp)
 app.register_blueprint(login_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
